19b65325e9e553be587cdc3368feffcc109725b3
wuhuizuo <wuhuizuo@126.com>
2021-06-23 19:35:25 +0800

    feat: add package `pkg/x` to export `experimental/registry.NewClient`

---
+++ pkg/x/registry.go
@@ -0,0 +1,7 @@
+// Package x is for utils.
+package x
+
+import "helm.sh/helm/v3/internal/experimental/registry"
+
+// NewRegistryClient export internal/experimental/registry.NewClient.
+var NewRegistryClient = registry.NewClient
